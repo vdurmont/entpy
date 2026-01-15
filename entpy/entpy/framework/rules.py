abf58c7a8202d7972eecc1b6199c9721e288edf5
Vincent Durmont <vincent@guild.ai>
2026-01-15 14:45:48 -0800

    Add AllOf privacy rule

--- entpy/entpy/framework/rules.py
+++ entpy/entpy/framework/rules.py
@@ -11,3 +11,19 @@ T = TypeVar("T", bound=Ent)
 class AllowAll(PrivacyRule):
     async def gen_evaluate(self, vc: ViewerContext, ent: T) -> Decision:
         return Decision.ALLOW
+
+
+class AllOf(PrivacyRule):
+    rules: list[PrivacyRule]
+
+    def __init__(self, rules: list[PrivacyRule]) -> None:
+        if len(rules) == 0:
+            raise ValueError("Cannot call `AllOf` rule with no rule to evaluate.")
+        self.rules = rules
+
+    async def gen_evaluate(self, vc: ViewerContext, ent: T) -> Decision:
+        for rule in self.rules:
+            decision = await rule.gen_evaluate(vc, ent)
+            if decision != Decision.ALLOW:
+                return Decision.DENY
+        return Decision.ALLOW
